8886246978fef52cb2e7aab6cdd73c398230bef2
Sobassy <altoscience1110@gmail.com>
2020-02-13 17:43:44 +0900

    1.0.7

---
+++ src/App_exhibition.js
@@ -0,0 +1,106 @@
+// rendererとipc通信を行う
+const {ipcRenderer} = require('electron')
+
+// 動的に作成したボタンから呼び出してCSV読みこみ
+global.csv_name = ""
+function load_csv(csv_file_name) {
+  ipcRenderer.send('load-csv', global.directory_name + csv_file_name)
+  global.csv_name = csv_file_name
+}
+
+// 全てリセット
+function reset_all() {
+  document.getElementById('selected-folder').innerHTML = 'フォルダが選択されていません'
+  document.getElementById('text-data').innerHTML = ''
+  document.getElementById('csv-list').innerHTML = ''
+}
+
+// ページ遷移
+const ChangeToExhibitionBtn = document.getElementById('exhibition')
+ChangeToExhibitionBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('change-to-exhibition')
+})
+const ChangeToManagerBtn = document.getElementById('buymanager')
+ChangeToManagerBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('change-to-manager')
+})
+const ChangeToScraperBtn = document.getElementById('scraper')
+ChangeToScraperBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('change-to-scraper')
+})
+const ChangeToImagerBtn = document.getElementById('imager')
+ChangeToImagerBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('change-to-imager')
+})
+
+// ストップ
+// function stop() {
+//   console.log('stop')
+//   ipcRenderer.removeAllListeners('start')
+// }
+
+// アカウントディレクトリ新規作成
+const makeDirBtn = document.getElementById('make-account-dir')
+makeDirBtn.addEventListener('click', (event) => {
+  var user_dir = document.getElementById('user-dir').value
+  ipcRenderer.send('make-account-dir', user_dir)
+})
+
+// フォルダ選択画面を呼び出し
+const selectDirBtn = document.getElementById('select-dir')
+selectDirBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('open-file-exhibition')
+})
+
+// ログ画面
+ipcRenderer.on('log-create', (event, log_text) => {
+  document.getElementById('logs').innerHTML += "<p>[log]: " + log_text + "</p>"
+  document.getElementById('footer').scrollTop = document.getElementById('footer').scrollHeight;
+})
+
+// 自動出品開始
+const StartBtn = document.getElementById('start-exhibition')
+StartBtn.addEventListener('click', (event) => {
+  // var mail = document.getElementById('mail').value
+  var args_list = {
+    "day_score": document.getElementById('day-score').value,
+    "hour_score": document.getElementById('hour-score').value,
+    "cookie": document.getElementById('access-code').value,
+    "dir_name": global.directory_name,
+    "csv": global.csv_name,
+    "url_memo": document.getElementsByName('url_memo').value
+  }
+  ipcRenderer.send('start-exhibition',args_list)
+})
+
+// 選択したディレクトリを表示
+ipcRenderer.on('selected-directory', (event, path) => {
+  global.directory_name = path.join(path, "/")
+  document.getElementById('selected-folder').innerHTML = `You selected: ${path}`
+})
+
+// 選択したディレクトリに保存されたアクセスコードを表示
+ipcRenderer.on('update-accesscode', (event, code) => {
+  document.getElementById('access-code').value = code
+})
+
+// 選択したcsvをボタンで表示
+ipcRenderer.on('selected-csv', (event, text_data) => {
+  document.getElementById('csv-list').innerHTML = `${text_data}`
+})
+
+// csvファイルの内容を表示
+ipcRenderer.on('selected-filedata', (event, text_data) => {
+  // ボタン分高さが変わるので自動調整
+  AutoAdjust()
+  document.getElementById('text-data').innerHTML = `${text_data}`
+})
+
+// パッディング自動調整
+function AutoAdjust() {
+  var padding = document.getElementsByClassName('csv-table')[0]
+  padding.style.paddingTop = document.getElementsByTagName('header')[0].offsetHeight
+  padding.style.paddingBottom = document.getElementsByTagName('footer')[0].offsetHeight
+}
+
+AutoAdjust()
---
+++ src/App_manager.js
@@ -0,0 +1,73 @@
+// rendererとipc通信を行う
+const {ipcRenderer} = require('electron')
+
+// デスクトップがあれば選択
+ipcRenderer.send('init-manager')
+
+// ページ遷移
+const ChangeToExhibitionBtn = document.getElementById('exhibition')
+ChangeToExhibitionBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('change-to-exhibition')
+})
+const ChangeToManagerBtn = document.getElementById('buymanager')
+ChangeToManagerBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('change-to-manager')
+})
+const ChangeToScraperBtn = document.getElementById('scraper')
+ChangeToScraperBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('change-to-scraper')
+})
+const ChangeToImagerBtn = document.getElementById('imager')
+ChangeToImagerBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('change-to-imager')
+})
+
+// フォルダ選択画面を呼び出し
+const selectDirBtn = document.getElementById('select-dir')
+selectDirBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('open-file-manager')
+})
+ 
+// 選択したディレクトリを表示
+ipcRenderer.on('selected-directory', (event, path) => {
+  global.directory_name = path + "/"
+  document.getElementById('selected-folder').innerHTML = `You selected: ${path}`
+})
+
+// Manager開始
+const StartBtn = document.getElementById('start-manager')
+StartBtn.addEventListener('click', (event) => {
+  var args_list = {
+    "page_setting": document.getElementById("page-choice").set1.value,
+    "onoff": document.getElementById('onoff').set2.value,
+    "days": document.getElementById('days').value,
+    "buyma_account": document.getElementById('buyma-account').value,
+    "buyma_dir": global.directory_name,
+    "access_prm":document.getElementById('access-prm').value,
+    "want_prm":document.getElementById('want-prm').value,
+    "cart_prm":document.getElementById('cart-prm').value,
+    "price_prm":document.getElementById('price-prm').value,
+    "maxprice_prm":document.getElementById('maxprice-prm').value,
+    "date_prm":document.getElementById('date-prm').value,
+  }
+
+  document.getElementById('logs').innerHTML += "<p>[log]: ページ選択：" + args_list["page_setting"] + "</p>"
+  document.getElementById('logs').innerHTML += "<p>[log]: 設定モード：" + args_list["onoff"] + "</p>"
+  document.getElementById('logs').innerHTML += "<p>[log]: 出品期間延長日数：" + args_list["days"] + "</p>"
+  ipcRenderer.send('start-manager', args_list)
+})
+
+// ログ画面
+ipcRenderer.on('log-create', (event, log_text) => {
+  document.getElementById('logs').innerHTML += "<p>[log]: " + log_text + "</p>"
+  document.getElementById('logs').scrollTop = document.getElementById('logs').scrollHeight;
+})
+
+// パッディング自動調整
+function AutoAdjust() {
+  var padding = document.getElementsByClassName('manager-logs')[0]
+  padding.style.paddingTop = document.getElementsByTagName('header')[0].offsetHeight
+}
+
+AutoAdjust()
+
